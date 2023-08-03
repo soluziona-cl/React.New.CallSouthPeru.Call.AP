bcf517595d4a75694fcfdef12e9cfec8c2d8d4f4
Soraxosolu <ignacio@soluziona.cl>
2023-08-03 15:13:33 -0400

    final

--- src/Despedida.js
+++ src/Despedida.js
@@ -5,6 +5,7 @@ const Despedida = () => {
             <br />
             Accidentes Personales con Ahorro
           </h1>
+          <p className="text-center"><h4>Registro Guardado Exitosamente</h4> </p>
   </>;
 };
 export default Despedida;
