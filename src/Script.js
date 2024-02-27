a064accd2b6878bc0c1386e9523488b6ad1048e8
FernandaFloresC <fernanda.flores.c.99@gmail.com>
2024-02-27 13:24:11 -0300

    script

--- src/Script.js
+++ src/Script.js
@@ -7,7 +7,6 @@ import es from "date-fns/locale/es";
 import "react-datepicker/dist/react-datepicker.css";
 
 
-
 registerLocale("es", es);
 
 function nobackbutton() {
@@ -25,49 +24,25 @@ const Index = () => {
   const campaign_file = urlParams.get("campaign");
   const [htmlFile, setHtmlFile] = useState(null);
 
-
-
-
   useEffect(() => {
-    if (campaign_file === 'Sonrie_Seguro') {
+    if (campaign_file === 'Chubb_AP') {
 
-      setHtmlFile('https://app.soluziona.cl/Orkesta/CallSouth/Ventas_V2/CallScript/htmlscript/script-campan-a-dental-everest.html')
-
+      setHtmlFile('https://app.soluziona.pe/Orkesta/CallSouth/Ventas/Script/SonrieSeguro.html')
 
     } 
-    // else if (campaign_file === 'Metlife_Dental_SanaSalud') {
-
-    //   setHtmlFile('https://app.soluziona.cl/Orkesta/CallSouth/Ventas_V2/CallScript/htmlscript/script-sanasalud-enero-2024.html')
-
-    // } else if (campaign_file === 'Metlife_Dental_UnoSalud') {
-
-    //   setHtmlFile('https://app.soluziona.cl/Orkesta/CallSouth/Ventas_V2/CallScript/htmlscript/Script_UnoSalud_Masiva_Enero_2024.html')
-
-    // } else if (campaign_file === 'Metlife_RGM_Entel') {
-
-    //   setHtmlFile('https://app.soluziona.cl/Orkesta/CallSouth/Ventas_V2/CallScript/htmlscript/RGM_MetLife_BAO.html')
-
-    // } else if (campaign_file === 'Metlife_APAhorro_Entel') {
-
-    //   setHtmlFile('https://app.soluziona.cl/Orkesta/CallSouth/Ventas_V2/CallScript/htmlscript/APAhorroEntel.html')
-    // }
-
 
   }, [campaign_file]);
 
   const iframeStyle = {
-    display: 'block',
-    marginLeft: '10%',
-    marginRight: '10%',
-    width: '1200px', // Make the iframe take the full width of its container
-    height: '1000px', // Set the desired height
+    maxWidth: '100vw', // Make the iframe take the full width of its container
+    height: '100vh', // Set the desired height
   };
   return (
     <>
 
       <div style={{ textAlign: 'center' }}>
         <div style={iframeStyle}>
-          <iframe src={htmlFile} title={htmlFile} width={1200} height={1000} ></iframe>
+          <iframe src={htmlFile} title={htmlFile} width={1300} height={1000}></iframe>
         </div>
       </div>
 
